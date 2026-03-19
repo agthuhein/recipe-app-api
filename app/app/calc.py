00e225cb0344364588cc814606cc91de0343213f
Aung Thu Hein <agthuhein.mm@gmail.com>
2026-03-19 21:24:18 +0100

    Added DB configuration in docker

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtract(a, b):
+    """Sub a and b and return result"""
+    return b - a
